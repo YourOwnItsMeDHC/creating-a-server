7feaad3dedd6d4aefd5cc4eaf5779a9a82f10d3c
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2023-01-26 14:23:03 +0530

    Created the server

---
+++ create-server/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const port = 7980;
+
+http.createServer((request, response) => {
+    //200 means, request is successful, and we are are sending your response
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>Hey Bro Welcome to Deepak Chourasiya's Server</h1>");
+    response.end();
+
+}).listen(port, () => {
+    console.log(`NodeJs server is started on ${port}`);
+});
+
+//It will get open on these link : http://localhost:7980
